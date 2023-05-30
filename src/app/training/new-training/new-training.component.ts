b9483f69c8ab4bd7e9d4ecdac930a57f8f6f30a9
soulaichhi <chhibou.soulaimane@gmail.com>
2023-05-30 16:01:48 +0100

    working with dummy data & add functionalities to training component

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,14 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TrainingService } from '../services/training.service';
+import { Exercise } from '../models/exercise.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.css'],
 })
-export class NewTrainingComponent {
-  @Output() trainingStart = new EventEmitter<void>();
+export class NewTrainingComponent implements OnInit {
+  exercises!: Exercise[];
 
-  onStartTraining() {
-    this.trainingStart.emit();
+  constructor(private trainingService: TrainingService) {}
+
+  ngOnInit() {
+    this.exercises = this.trainingService.getAvailableExercises();
+  }
+
+  onStartTraining(form: NgForm) {
+    this.trainingService.startExercise(form.value.exercise);
   }
 }
--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { TrainingService } from './services/training.service';
 
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
   styleUrls: ['./training.component.css'],
 })
-export class TrainingComponent {
+export class TrainingComponent implements OnInit {
   ongoingTraining = false;
+  exerciseSubscription!: Subscription;
+
+  constructor(private trainingService: TrainingService) {}
+
+  ngOnInit() {
+    this.exerciseSubscription = this.trainingService.exerciseChanged.subscribe(
+      (exercise) => {
+        if (exercise) {
+          this.ongoingTraining = true;
+        } else {
+          this.ongoingTraining = false;
+        }
+      }
+    );
+  }
 }
